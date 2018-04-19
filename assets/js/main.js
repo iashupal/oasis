0afd1756ea49485a3a0f6a247ce896b108d007f2
ashupal <ashupal0005@gmail.com>
2018-04-19 12:54:43 +0530

    oasis

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            dots: true
+        },
+        480: {
+            items: 2,
+            dots: true
+        }
+    }
+})
